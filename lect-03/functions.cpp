c3b1e80c96d7731da87030c06dc1cce4b43b62eb
Diba Mirza <dibamirza@169-231-222-20.wireless.ucsb.edu>
2020-01-14 16:47:21 -0800

    lecture 3

--- lect-03/functions.cpp
+++ lect-03/functions.cpp
@@ -1,10 +1,24 @@
 #include <iostream>
 using namespace std;
 
-void printHello();
+void printHello(); //delcare the function
+string retHello();
 
 int main(){
-
+    string getResult; //delcare your variables
+    printHello();
+    cout<<getResult;
+    getResult= retHello() + " CS16";
+   // getResult = getResult + " CS16";   
+    cout<<getResult<<endl;
     return 0;
+}
+
+void printHello(){ //define the function
+    cout<<"Hello\n";
+}
+
+string retHello(){ //define the function   
+    return "Hello";
 }
 
